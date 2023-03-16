0fedb82276ffc51b836436652c43111d2c651fe3
shivamj291 <shivamj291@gmail.com>
2023-03-16 14:35:42 +0530

    resume name change

--- src/component/Front/Front.js
+++ src/component/Front/Front.js
@@ -23,7 +23,7 @@ function Front(){
                         <a href="https://www.linkedin.com/in/shivam-jaiswal-43364a23b"><img src="https://cdn-icons-png.flaticon.com/512/174/174857.png"/></a>
                         <a href="shivamj291@gmail.com"><img src="https://cdn4.iconfinder.com/data/icons/social-media-logos-6/512/112-gmail_email_mail-512.png"/></a>
                     </div>
-                   <div id="resume"><a href="resume.pdf"><p>Resume</p></a></div>
+                   <div id="resume"><a href="shivam_jaiswal.pdf"><p>Resume</p></a></div>
 
                    
                    
