7c3f105a51d045d3f1662b6705759a5122634501
shivamj291 <shivamj291@gmail.com>
2023-03-22 03:27:04 +0530

    ready

--- src/Separate/Profile/Profile.jsx
+++ src/Separate/Profile/Profile.jsx
@@ -5,8 +5,15 @@ import style from "./Profile.module.css"
 function Profile(){
      return(
         <div id={style.Profile}  >
-            <img src="shivam.jpg" id={style.pimg}/>
-            <img src="https://readme-typing-svg.herokuapp.com?font=Architects+Daughter&amp;color=ff0000&amp;size=20&amp;lines=👋Hey!+It's+SHIVAM+JAISWAL!;Full+Stack-Web+Developer...👨🏻‍💻;"/>
+            <div className={style.outerP}>
+                    <div className={style.innerP}>
+                       <img src="shivam.jpg" id={style.pimg}/>
+                       <span> <img src="https://readme-typing-svg.herokuapp.com?font=Architects+Daughter&amp;color=ff0000&amp;size=20&amp;lines=👋Hey!+It's+SHIVAM+JAISWAL!;Full+Stack-Web+Developer...👨🏻‍💻;" id={style.full}/></span>
+           
+                    </div>
+                </div>
+           
+         
         </div>
     )
 }
